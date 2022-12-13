84e323524257fc2c42510e4481aeae1fdc37291b
Andreotti Ludovic <andr.ludow@gmail.com>
2022-12-13 11:41:39 +0100

    tacke choice

--- src/main/java/com/andreotti/ludovic_todo/App.java
+++ src/main/java/com/andreotti/ludovic_todo/App.java
@@ -1,12 +1,14 @@
 package com.andreotti.ludovic_todo;
 
 import com.andreotti.ludovic_todo_controller.Todo_vue;
+import com.andreotti.ludovic_todo_data.Todo_storage;
 
 public class App {
 
 	public static void main(String[] args) {
 
 		Todo_vue.askQuestion();
+		Todo_storage.showStorage();
 
 	}
 
--- src/main/java/com/andreotti/ludovic_todo_controller/Todo_vue.java
+++ src/main/java/com/andreotti/ludovic_todo_controller/Todo_vue.java
@@ -1,22 +1,16 @@
 package com.andreotti.ludovic_todo_controller;
 
-import java.util.Scanner;
-
 public class Todo_vue {
-
-	static Scanner sc = new Scanner(System.in);
 
 	public static void askQuestion() {
 		System.out.println("Bienvenue dans votre liste de tâches.");
 		System.out.println("Que souhaitez vous faire ?");
+		showChoices();
 	}
 
 	public static void showChoices() {
-//		int choice;
-
-		System.out.println("1- Ajouter une tâche\n 2- Retirer une tâche\n 3- Voir mes tâches");
-
-		choice = sc.nextInt();
+		System.out.println("1- Ajouter une tâche\n2- Retirer une tâche\n3- Voir mes tâches");
+		Todo_action.takeChoice();
 
 	}
 
